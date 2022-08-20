df4bf003d0466efa9ccab4a5d11c36b740ec6d78
EunJung803 <aimee3322@gmail.com>
2022-08-21 07:29:22 +0900

    Feat : player ready in the room

--- src/container/game/GameContainer.js
+++ src/container/game/GameContainer.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Button } from '@mui/material';
+import { Button, Paper } from '@mui/material';
 
 import RoomInfo from "./RoomInfo";
+import "./gamecont.scss";
 
 const GameContainer = ({ socket, room_info }) => {
 
@@ -9,7 +10,7 @@ const GameContainer = ({ socket, room_info }) => {
     room_info:
     map: "hello"
     player: Array(2)
-    0: (2) ['GG6OQ4FWXKjAH2SfAAAD', 0]
+    0: (2) ['GG6OQ4FWXKjAH2SfAAAD', 0]  // -> [socket.id, ready상태] -> 0 : 준비 X / 1 : 준비 O
     1: (2) ['wE3Cf2G6qZx9AtQjAAAH', 0]
     length: 2
     [[Prototype]]: Array(0)
@@ -20,9 +21,13 @@ const GameContainer = ({ socket, room_info }) => {
     const exitRoom = () => socket.emit("exit_room");
 
     return (
-        <div>
-            <Button variant="contained" onClick={exitRoom}>되돌아가기</Button>
-            {/* <RoomInfo socket={socket} {...room_info}/> */}
+        <div className="exitRoom">
+            <Paper elevation={3} className="paper" justifyContent="center"> 
+                <Button variant="contained" onClick={exitRoom}>되돌아가기</Button>
+                <Paper>
+                    <RoomInfo socket={socket} {...room_info}/>
+                </Paper>
+            </Paper>            
         </div>
     );
 
