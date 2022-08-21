9162db54db95b85e41c0ba0d0d297d46da638f4f
EunJung803 <aimee3322@gmail.com>
2022-08-21 10:40:39 +0900

    Feat : Joystick char moving in GameInfo

--- src/container/game/GameInfo.js
+++ src/container/game/GameInfo.js
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
+import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
+import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp';
+import ArrowCircleDownIcon from '@mui/icons-material/ArrowCircleDown';
 
 import RoomMain from "./RoomMain";
 import "./RoomMain.scss"
 
 const GameInfo = ({ socket, room_id, player, turn}) => {
+
+
+    var [lrindex, setNumber] = useState(0)
+    var [udindex, setNum] = useState(0)
+    const goLeft = () => {
+        setNumber(lrindex-50);
+        console.log("left lrindex", lrindex)
+    };
+
+    const goRight = () => {
+        setNumber(lrindex+50);
+        console.log("rigth lrindex",lrindex)
+
+    };
+
+    const goUp = () => {
+        setNum(udindex-50);
+        console.log("udindex",udindex)
+
+    };
+
+    const goDown = () => {
+        setNum(udindex+50);
+        console.log("dd udindex" ,udindex)
+
+    };
+
+    var ctrans = 'translate('+lrindex+'px, '+udindex+'px)'
+    var css = {
+        transform : ctrans
+    }
 
     // TODO
     // useeffect -> 렌더링할 때 실행되게 해야한다 gameinfo 정보가 왔을 때
@@ -13,7 +49,15 @@ const GameInfo = ({ socket, room_id, player, turn}) => {
         <div>
             <RoomMain />
             <div>
-                <img className="paprica" alt="y_paprica_img" src={`${process.env.PUBLIC_URL}/asset/Yellow Paprica gif/front-walk.gif`}></img>
+                <img className="my_paprica" style= {css} 
+                alt="y_paprica_img" src={`${process.env.PUBLIC_URL}/asset/Yellow Paprica gif/front-walk.gif`}></img>
+            </div>
+
+            <div className="joystick">
+                <ArrowCircleLeftIcon onClick={goLeft} />
+                <ArrowCircleRightIcon onClick={goRight} />
+                <ArrowCircleUpIcon onClick={goUp}/>
+                <ArrowCircleDownIcon onClick={goDown}/>
             </div>
         </div>
     );
--- src/container/game/RoomMain.js
+++ src/container/game/RoomMain.js
@@ -1,47 +1,15 @@
 import React, { useState } from "react";
 import { Button, Box } from "@mui/material";
-import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
-import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
-import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp';
-import ArrowCircleDownIcon from '@mui/icons-material/ArrowCircleDown';
+
 
 import styles from "./RoomMain.scss"
 
-// 맵을 띄우는 파일
 
+// 맵을 띄우는 파일
 export default function RoomMain(){
+
     const widthSize = 1500;
     const heightSize = 800;
-
-    var [lrindex, setNumber] = useState(0)
-    var [udindex, setNum] = useState(0)
-    const goLeft = () => {
-        setNumber(lrindex-50);
-        console.log("left lrindex", lrindex)
-    };
-
-    const goRight = () => {
-        setNumber(lrindex+50);
-        console.log("rigth lrindex",lrindex)
-
-    };
-
-    const goUp = () => {
-        setNum(udindex-50);
-        console.log("udindex",udindex)
-
-    };
-
-    const goDown = () => {
-        setNum(udindex+50);
-        console.log("dd udindex" ,udindex)
-
-    };
-
-    var ctrans = 'translate('+lrindex+'px, '+udindex+'px)'
-    var css = {
-        transform : ctrans
-    }
 
     return(
         <div className="roomMain">
@@ -52,14 +20,6 @@ export default function RoomMain(){
                 }}
                 alt="school_img" src={`${process.env.PUBLIC_URL}/asset/background.png`}/>
 
-                <img className="my_paprica" style= {css} 
-                alt="y_paprica_img" src={`${process.env.PUBLIC_URL}/asset/Yellow Paprica gif/front-walk.gif`}></img>
-            </div>
-            <div className="joystick">
-                <ArrowCircleLeftIcon onClick={goLeft} />
-                <ArrowCircleRightIcon onClick={goRight} />
-                <ArrowCircleUpIcon onClick={goUp}/>
-                <ArrowCircleDownIcon onClick={goDown}/>
             </div>
 
         </div>
